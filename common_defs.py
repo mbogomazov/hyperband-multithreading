8a847e09a5b69a145f4ab72d0fddb9fa4e59c798
anyaschenikova <58432297+anyaschenikova@users.noreply.github.com>
2021-10-30 17:17:30 +0300

    Update common_defs.py

--- common_defs.py
+++ common_defs.py
@@ -86,7 +86,7 @@ def train_and_eval_sklearn_regressor( clf, data ):
 	mse = MSE( y_train, p )
 	rmse = sqrt( mse )
 	mae = MAE( y_train, p )
-  r2_score = r2(y_train, p)
+  	r2_score = r2(y_train, p)
 
 
 	print ("\n# training | RMSE: {:.4f}, MAE: {:.4f}, R2: {:.4f}".format( rmse, mae, r2_score ))
@@ -98,7 +98,7 @@ def train_and_eval_sklearn_regressor( clf, data ):
 	mse = MSE( y_test, p )
 	rmse = sqrt( mse )
 	mae = MAE( y_test, p )
-  r2_score = r2(y_test, p)
+  	r2_score = r2(y_test, p)
 
 	print ("# testing  | RMSE: {:.4f}, MAE: {:.4f}, R2: {:.4f}".format( rmse, mae, r2_score ))	
 	
