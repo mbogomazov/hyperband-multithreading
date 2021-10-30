7e34257c309e9847e2e3c8e3bbf1ebce90b8033d
anyaschenikova <58432297+anyaschenikova@users.noreply.github.com>
2021-10-30 15:50:22 +0300

    Update load_data_for_regression.py

--- load_data_for_regression.py
+++ load_data_for_regression.py
@@ -5,7 +5,7 @@ file = '/content/airfoil_self_noise.csv'
 # valid_file = 'data/kin8nm/validation.csv'
 # test_file = 'data/kin8nm/test.csv'
 
-data_2 = pd.read_csv('/content/airfoil_self_noise.csv',sep='\t')
+data_2 = pd.read_csv('/content/airfoil_self_noise.csv')
 
 X = data_2.drop('L',axis=1) # Independet variable
 y = data_2['L'] # Dependent variable
