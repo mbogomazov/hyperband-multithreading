b5db21078c2a3a5014e6fafa4fdf5a3f1bde06ab
anyaschenikova <58432297+anyaschenikova@users.noreply.github.com>
2021-10-30 13:16:24 +0300

    Update polylearn_fm.py

--- defs_regression/polylearn_fm.py
+++ defs_regression/polylearn_fm.py
@@ -1,104 +1,62 @@
-"imports and definitions shared by various defs files"
+"function (and parameter space) definitions for hyperband"
+"binary classification with polylearn FM"
 
-import numpy as np
+from hyperband.common_defs import *
 
-from math import log, sqrt
-from time import time
-from pprint import pprint
+# a dict with x_train, y_train, x_test, y_test
+from hyperband.load_data_for_regression import data
 
-from sklearn.metrics import roc_auc_score as AUC, log_loss, accuracy_score as accuracy
-from sklearn.metrics import mean_squared_error as MSE, mean_absolute_error as MAE
+from polylearn import FactorizationMachineRegressor as FM
+from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler, MaxAbsScaler
 
-try:
-	from hyperopt import hp
-	from hyperopt.pyll.stochastic import sample
-except ImportError:
-	print ("In order to achieve operational capability, this programme requires hyperopt to be installed (pip install hyperopt), unless you make get_params() use something else.")
+#
+
+iters_per_iteration = 1
+
+space = {
+	'scaler': hp.choice( 's', 
+		( None, 'StandardScaler', 'RobustScaler', 'MinMaxScaler', 'MaxAbsScaler' )),	
 	
-#	
+	'degree': hp.quniform( 'd', 2, 3, 1 ),
+	'n_components': hp.quniform( 'c', 1, 50, 1 ),
+	'alpha': hp.uniform( 'a', 1e-10, 2 ),
+	'beta': hp.uniform( 'b', 1e-10, 2 ), 
+	'fit_lower': hp.choice( 'flo', ( 'augment', 'explicit', None )),
+	'fit_linear': hp.choice( 'fln', ( False, True )),
+	'init_lambdas': hp.choice( 'il', ( 'ones', 'random_signs' ))
+}
 
-# handle floats which should be integers
-# works with flat params
-def handle_integers( params ):
+def get_params():
 
-	new_params = {}
-	for k, v in params.items():
-		if type( v ) == float and int( v ) == v:
-			new_params[k] = int( v )
-		else:
-			new_params[k] = v
-	
-	return new_params
-	
-###
+	params = sample( space )
+	return handle_integers( params )
 
-def train_and_eval_sklearn_classifier( clf, data ):
-	
-	x_train = data['x_train']
-	y_train = data['y_train']
-	
-	x_test = data['x_test']
-	y_test = data['y_test']	
-	
-	clf.fit( x_train, y_train )	
-	
-	try:
-		p = clf.predict_proba( x_train )[:,1]	# sklearn convention
-	except IndexError:
-		p = clf.predict_proba( x_train )
+#
 
-	ll = log_loss( y_train, p )
-	auc = AUC( y_train, p )
-	acc = accuracy( y_train, np.round( p ))
-
-	print ("\n# training | log loss: {:.2%}, AUC: {:.2%}, accuracy: {:.2%}".format( ll, auc, acc ))
-
+def try_params( n_iterations, params ):
+	
+	max_iter = int( round( n_iterations * iters_per_iteration ))
+	print ("max_iter:", max_iter)
+	pprint( params )
+	
+	if params['scaler']:
+		scaler = eval( "{}()".format( params['scaler'] ))
+		x_train_ = scaler.fit_transform( data['x_train'].astype( np.float64 ))
+		x_test_ = scaler.transform( data['x_test'].astype( np.float64 ))
+	else:
+		x_train_ = data['x_train'].astype( np.float64 )
+		x_test_ = data['x_test'].astype( np.float64 )
+		
+	y_train_ = data['y_train'].copy()
+	y_test_ = data['y_test'].copy()
+	
+	local_data = { 'x_train': x_train_, 'y_train': y_train_, 
+		  'x_test': x_test_ , 'y_test': y_test_ }
+	
 	#
-
-	try:
-		p = clf.predict_proba( x_test )[:,1]	# sklearn convention
-	except IndexError:
-		p = clf.predict_proba( x_test )
-
-	ll = log_loss( y_test, p )
-	auc = AUC( y_test, p )
-	acc = accuracy( y_test, np.round( p ))
-
-	print ("# testing  | log loss: {:.2%}, AUC: {:.2%}, accuracy: {:.2%}".format( ll, auc, acc ))	
 	
-	#return { 'loss': 1 - auc, 'log_loss': ll, 'auc': auc }
-	return { 'loss': ll, 'log_loss': ll, 'auc': auc }
-
-###
-
-# "clf", even though it's a regressor
-def train_and_eval_sklearn_regressor( clf, data ):
+	params_ = dict( params )
+	params_.pop( 'scaler' )	
 	
-	x_train = data['x_train']
-	y_train = data['y_train']
-	
-	x_test = data['x_test']
-	y_test = data['y_test']	
-	
-	clf.fit( x_train, y_train )	
-	p = clf.predict( x_train )
-
-	mse = MSE( y_train, p )
-	rmse = sqrt( mse )
-	mae = MAE( y_train, p )
-
-
-	print ("\n# training | RMSE: {:.4f}, MAE: {:.4f}".format( rmse, mae ))
-
-	#
-
-	p = clf.predict( x_test )
-
-	mse = MSE( y_test, p )
-	rmse = sqrt( mse )
-	mae = MAE( y_test, p )
-
-	print ("# testing  | RMSE: {:.4f}, MAE: {:.4f}".format( rmse, mae ))	
-	
-	return { 'loss': rmse, 'rmse': rmse, 'mae': mae }
-
+	clf = FM( max_iter = max_iter, verbose = True, **params_ )
+	return train_and_eval_sklearn_regressor( clf, local_data )
