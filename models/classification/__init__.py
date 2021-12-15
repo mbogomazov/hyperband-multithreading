fca8131825b8917d94be9a8329b38322fa399ae2
NoName <ihackit595@gmail.com>
2021-12-16 01:02:10 +0300

    add package folder structure

---
+++ models/classification/__init__.py
@@ -0,0 +1,4 @@
+from base_classification_model import  BaseClassificationModel
+from rf import HBRandomForestClassifier
+from gb import HBGradientBoostingClassifier
+from xgb import HBXGBoostClassification
